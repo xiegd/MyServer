fbeeb5b87d21a97b87971d9bc919fc9707b5a169
xgd <xgd0401@gmail.com>
2024-10-28 19:25:09 +0800

    complete thread part

---
+++ src/thread/threadpool.cc
@@ -0,0 +1,169 @@
+#include "threadpool.h"
+
+#include <stdexcept>
+#include "logger.h"
+#include "msgqueue.h"
+#include "taskexecutor.h"
+#include "utility.h"
+
+
+namespace xkernel {
+
+//////////////////////////////////// ThreadGroup //////////////////////////////////////
+
+ThreadGroup::~ThreadGroup() { threads_.clear();}
+
+bool ThreadGroup::isThisThreadIn() {
+    auto thread_id = std::this_thread::get_id();
+    if (thread_id == thread_id_) {
+        return true;  // 如果是最后创建的线程
+    }
+    return threads_.find(thread_id) != threads_.end();
+}
+
+bool ThreadGroup::isThreadIn(std::thread* thrd) {
+    if (!thrd) {
+        return false;
+    }
+    return threads_.find(thrd->get_id()) != threads_.end();
+}
+
+void ThreadGroup::removeThread(std::thread* thrd) {
+    auto it = threads_.find(thrd->get_id());
+    if (it != threads_.end()) {
+        threads_.erase(it);
+    }
+}
+
+void ThreadGroup::joinAll() {
+    if (isThisThreadIn()) {
+        throw std::runtime_error("Trying joining itself in thread_group");
+    }
+    for (auto& it : threads_) {
+        if (it.second->joinable()) {
+            it.second->join();
+        }
+    }
+    threads_.clear();
+}
+
+size_t ThreadGroup::size() { return threads_.size(); }
+
+//////////////////////////////////// ThreadPool //////////////////////////////////////
+
+ThreadPool::ThreadPool(int num, Priority priority, bool auto_run, 
+                       bool set_affinity, const std::string& pool_name) {
+    thread_num_ = num;
+    on_setup_ = [pool_name, priority, set_affinity](int index) {
+        std::string name = pool_name + '' + std::to_string(index);
+        setPriority(priority);
+        ThreadUtil::setThreadName(name.data());
+        if (set_affinity) {
+            ThreadUtil::setThreadAffinity(index % std::thread::hardware_concurrency());
+        }
+    };
+    logger_ = Logger::Instance().shared_from_this();
+    if (auto_run) {
+        start();
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    shutdown();
+    wait();
+}
+    
+Task::Ptr ThreadPool::async(TaskIn task, bool may_sync) {
+    if (may_sync && thread_group_.is_this_thread_in()) {
+        task();
+        return nullptr;
+    }
+    auto ret = std::make_shared<Task>(std::move(task));
+    queue_.pushMsg(ret);
+    return ret;
+}
+
+Task::Ptr ThreadPool::async_first(TaskIn task, bool may_sync = true) {
+    if (may_sync && thread_group_.is_this_thread_in()) {
+        task();
+        return nullptr;
+    }
+    auto ret = std::make_shared<Task>(std::move(task));
+    queue_.pushMsgToHead(ret);
+    return ret;
+}
+
+size_t ThreadPool::size() { return queue_.size(); }
+
+bool ThreadPool::setPriority(Priority priority, std::thread::native_handle_type threadId) {
+    static int Min = sched_get_priority_min(SCHED_FIFO);  // 获取SCHED_FIFO调度策略的最小优先级
+    if (Min == -1) {
+        return false;
+    }
+    static int Max = sched_get_priority_max(SCHED_FIFO);  // 获取SCHED_FIFO调度策略的最大优先级
+    if (Max == -1 ) {
+        return false;
+    }
+    static int Priorities[] {Min, Min + (Max - Min)/4,
+                            Min + (Max - Min)/2,
+                            Min + (Max - Min) * 3/4, Max};
+    if (threadId == 0) {
+        threadId = pthread_self();
+    }
+    struct sched_param params;  // 创建调度参数结构体
+    params.sched_priority = Priorities[priority];
+    return pthread_setschedparam(threadId, SCHED_FIFO, &params) == 0;
+}
+
+void ThreadPool::start() {
+    if (thread_num_ <= 0) {
+        return ;
+    }
+    size_t total = thread_num_ - thread_group_.size();
+    for (size_t i = 0; i < total; ++i) {
+        thread_group_.create_thread([this, i](){ run(i); })
+    }
+}
+
+void ThreadPool::run(size_t index) {
+    on_setup_(index);
+    Task::Ptr task;
+    while (true) {
+        startSleep();
+        if (!queue_.getMsg(task)) {
+            break;  // 空任务，退出线程
+        }
+        sleepWakeUp();
+        try {
+            (*task)();
+            task = nullptr;
+        } catch (std::exception& ex) {
+            ErrorL << "ThreadPool catch a exception: " << ex.what();
+        }
+    }
+}
+
+void ThreadPool::wait() { thread_group_.joinAll(); }
+void ThreadPool::shutdown() { queue_.push_exit(thread_num_); }
+
+//////////////////////////////////// ThreadPool //////////////////////////////////////
+
+static size_t s_pool_size = 0;
+static bool s_enable_cpu_affinity = true;
+
+INSTANCE_IMP(WorkThreadPool)
+
+// 设置线程池大小, 必须在WorkThreadPool单例创建前调用
+void WorkThreadPool::setPoolSize(size_t size) { s_pool_size = size; }
+
+void WorkThreadPool::enableCpuAffinity(bool enable) { s_enable_cpu_affinity = enable; }
+
+EventPoller::Ptr WorkThreadPool::getPoller() {
+    return std::static_pointer_cast<EventPoller>(getExecutor());
+}
+
+EventPoller::Ptr WorkThreadPool::getFirstPoller() {
+    return std::static_pointer_cast<EventPoller>(threads_.front());
+}
+
+}  // namespace xkernel
--- src/thread/threadpool.h
+++ src/thread/threadpool.h
@@ -1,9 +1,44 @@
 #ifndef _THREADPOOL_H_
 #define _THREADPOOL_H_
 #include <stddef.h>
+#include <thread>
+#include <unordered_map>
+#include <memory>
 
 #include "taskexecutor.h"
 #include "logger.h"
+#include "msgqueue.h"
+#include "eventpoller.h"
+
+namespace xkernel {
+
+// 线程组类，管理一组线程
+class ThreadGroup {
+public:
+    ThreadGroup() = default;
+    ThreadGroup(const ThreadGroup&) = delete;
+    ~ThreadGroup();
+    ThreadGroup& operator=(const ThreadGroup&) = delete;
+
+public:
+    bool isThisThreadIn();                 // 判断当前线程是否在线程组中
+    bool isThreadIn(std::thread* thrd);    // 判断指定线程是否在线程组中
+    void removeThread(std::thread* thrd);  // 从线程组中移除指定线程
+    void joinAll();                        // 等待所有线程结束
+    size_t size();                         // 获取线程组中的线程数量
+    // 创建新线程并添加到线程组
+    template <typename F>
+    std::thread* createThread(F&& threadfunc) {
+        auto thread_new = std::make_shared<std::thread>(std::forward<F>(threadfunc));
+        thread_id_ = thread_new->get_id();
+        threads_[thread_id_] = thread_new;
+        return thread_new.get();
+    }
+
+private:
+    std::thread::id thread_id_;            // 最后创建的线程ID
+    std::unordered_map<std::thread::id, std::shared_ptr<std::thread>> threads_;  // 线程ID到线程对象的映射表
+};
 
 class ThreadPool : public TaskExecutor {
 public:
@@ -29,16 +64,35 @@ public:
     void start();
 
 private:
-    void run();
+    void run(size_t index);
     void wait();
     void shutdown();
 
 private:
     size_t thread_num_;
     Logger::Ptr logger_;
-    thread_group thread_group_;
-    TaskQueue<Task::Ptr> queue_;
+    ThreadGroup thread_group_;
+    MsgQueue<Task::Ptr> queue_;
     std::function<void(int)> on_setup_;
-}
+};
+
+class WorkThreadPool : public std::enable_shared_from_this<WorkThreadPool>,
+                       public TaskExecutorGetterImp {
+public:
+    using Ptr = std::shared_ptr<WorkThreadPool>;
+    WorkThreadPool() override = default;
+    static WorkThreadPool& Instance();
+
+public:
+    static void setPoolSize(size_t size = 0);  // 设置
+    static void enableCpuAffinity(bool enable);
+    EventPoller::Ptr getFirstPoller();
+    EventPoller::Ptr getPoller();
+
+protected:
+    WorkThreadPool();
+};
+
+}  // namespace xkernel
 
 #endif
