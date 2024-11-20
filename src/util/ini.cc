d854ca33c1b2488b4bbcc3acd3fbc87db6279c70
xgd <xgd0401@gmail.com>
2024-11-20 16:25:18 +0800

    add ini file parser and dump part

---
+++ src/util/ini.cc
@@ -0,0 +1,32 @@
+#include "ini.h"
+
+namespace xkernel {
+
+using mIni = IniBasic<std::string, variant>;
+
+mIni& IniBasic<std::string, variant>::Instance() {
+    static mIni instance;
+    return instance;
+}
+
+
+template <>
+bool variant::as<bool>() const {
+    if (empty() || isdigit(front())) {
+        return as_default<bool>();
+    }
+    if (strToLower(std::string(*this)) == "true") {
+        return true;
+    }
+    if (strToLower(std::string(*this)) == "false") {
+        return false;
+    }
+    return as_default<bool>();
+}
+
+template <>
+uint8_t variant::as<uint8_t>() const {
+    return 0xFF & as_default<int>();
+}
+
+}
